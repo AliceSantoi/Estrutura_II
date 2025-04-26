de2138c5cace143f3cbb7563a391a208b445cb4a
AliceSantoi <alicelira2016@gmail.com>
2025-04-26 16:15:29 -0300

    adicionando atividade
    potencia sem usar o pow

--- Exercícios _Programação_C/função_e_modularização/numeros_primos_em_intervalo.c
+++ Exercícios _Programação_C/função_e_modularização/numeros_primos_em_intervalo.c
@@ -1,4 +1,4 @@
-/*Escreva uma função `bool ehPrimo(int n)` e use-a para listar todos 
+/*Escreva uma função `bool ehPrimo(int n)` e use-a para listar todos
 os primos entre dois números dados pelo usuário.
 */
 
@@ -9,54 +9,59 @@ os primos entre dois números dados pelo usuário.
 bool ehPrimo(int n);
 void imprimeIntervalo(int n1, int n2);
 
-int main(){
+int main()
+{
     int numberOne, numberTwo;
 
-    do{
+    do
+    {
         printf("Digite o primeiro numero: ");
-        scanf("%d",&numberOne);
+        scanf("%d", &numberOne);
 
         printf("Digite o segundo numero: ");
-        scanf("%d",&numberTwo);
-    }while(numberOne <= 0 || numberTwo <=0);
+        scanf("%d", &numberTwo);
+    } while (numberOne <= 0 || numberTwo <= 0);
 
-    imprimeIntervalo(numberOne,numberTwo);
+    imprimeIntervalo(numberOne, numberTwo);
 
     return 0;
 }
 
-
- bool ehPrimo(int n){
+bool ehPrimo(int n)
+{
     int cont = 0;
-    if(n <= 1){
+    if (n <= 1)
+    {
         return false;
     }
 
-    for(int i = 2; i <= n/2;i++){
-        if(n % i == 0){
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
             cont++;
             break;
         }
-    }  
+    }
 
-    if(cont == 0){
+    if (cont == 0)
+    {
         return true;
     }
     return false;
-    
- }
+}
 
- void imprimeIntervalo(int n1, int n2){
-    while(n1 <= n2){
-        if(ehPrimo(n1)){
-            printf("%d",n1);
+void imprimeIntervalo(int n1, int n2)
+{
+    while (n1 <= n2)
+    {
+        if (ehPrimo(n1))
+        {
+            printf("%d", n1);
         }
         n1++;
-      
     }
 
     printf("\n");
     return;
-
-
- }
+}
