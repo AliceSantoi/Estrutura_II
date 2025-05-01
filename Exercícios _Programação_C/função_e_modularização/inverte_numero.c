3a1b70ff20191f2e64455fb962f92809133ffb07
AliceSantoi <alicelira2016@gmail.com>
2025-04-30 23:01:45 -0300

    adicionando atividades

---
+++ Exercícios _Programação_C/função_e_modularização/inverte_numero.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void inverteNum(int num);
+
+int main()
+{
+    inverteNum(123);
+
+    return 0;
+}
+
+void inverteNum(int num)
+{
+    int acumulador = 0;
+    if (num >= 0)
+    {
+        do
+        {
+            acumulador = num % 10;
+            printf("%d", acumulador);
+            num /= 10;
+        } while (num != 0);
+
+        printf("\n");
+    }
+}
---
+++ Exercícios _Programação_C/vetores_e_logica/frequencia_numeros.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int vet[10];
+
+    for(int index = 0;index < 10;index++){
+        printf("- ");
+        scanf("%d",&vet[index]);
+    }
+
+    //
+
+    return 0;
+}
