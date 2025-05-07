1aa267bbde83eec17670d02a1958a34dd4d612f8
AliceSantoi <alicelira2016@gmail.com>
2025-05-07 15:16:38 -0300

    adicionando atividade

--- Exercícios _Programação_C/vetores_e_logica/intersecao_de_vetores.c
+++ Exercícios _Programação_C/vetores_e_logica/intersecao_de_vetores.c
@@ -2,14 +2,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+const int TAMANHO = 8;
 
-int* mesmosValores(int* vetOne, int* vetTwo);
+int *mesmosValores(int *vetOne, int *vetTwo);
 
-int main(){
-
-
-
-
+int main()
+{
+    int vetOne[TAMANHO], vetTwo[TAMANHO];
+    for (int i = 0; i < TAMANHO; i++)
+    {
+        printf("valor %d: ", (i + 1));
+        scanf("%d",&vetOne[i]);
+    }
+    int *comuns = mesmosValores(vetOne,vetTwo);
 
     return 0;
+}
+
+int *mesmosValores(int *vetOne, int *vetTwo)
+{
+
 }
